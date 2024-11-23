391023da535867b0324facd096e0dbc7ab83cc31
Pedro <pedromonrealsanchez@gmail.com>
2024-11-23 16:34:44 +0100

    actualizar

--- PHP + HTML/Bin-Dec_converter.php
+++ PHP + HTML/Bin-Dec_converter.php
@@ -8,54 +8,65 @@
 </head>
 
 <body>
+
+    <?php
+    $binaryVALUE = "";
+    $decimalVALUE = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (!isset($_POST["decimal"]) && !isset($_POST["binary"]) || empty($_POST["decimal"]) && empty($_POST["binary"])) {
+            echo "Empty values";
+        } elseif (!empty($_POST["decimal"]) && !empty($_POST["binary"])) {
+            echo "Least one value must be empty";
+        } elseif (empty($_POST["decimal"])) {
+            if (empty(str_replace(['0', '1'], '', $_POST["binary"]))) {
+                //echo $_POST["binary"] . " equival a " . binaryTOdecimal($_POST["binary"]);
+                $binaryVALUE = $_POST["binary"];
+                $decimalVALUE = binaryTOdecimal($_POST["binary"]);
+            } else {
+                echo "Invalid binary number";
+            }
+        } elseif (empty($_POST["binary"])) {
+            //echo $_POST["decimal"] . " equival a " . decimalTObinary($_POST["decimal"]);
+            $decimalVALUE = $_POST["decimal"];
+            $binaryVALUE = decimalTObinary($_POST["decimal"]);
+        }
+    }
+
+
+    function decimalTObinary($value)
+    {
+        return decbin($value);
+    }
+
+    function binaryTodecimal($value)
+    {
+        return bindec($value);
+    }
+
+    function inputvalue($type, $binary, $decimal)
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if ($type == "decimal") {
+                echo $decimal;
+            } else {
+                echo $binary;
+            }
+        }
+    }
+
+
+    ?>
+
     <h1 align="center">Bin - Dec converter</h1>
-    <form align="center" action="" method="POST">   
+    <form align="center" action="" method="POST">
         <label for="decimal">Decimal</label>
-        <input type="number" name="decimal" value="<?php inputvalue("decimal")?>">
+        <input type="number" name="decimal" value="<?php inputvalue("decimal", $binaryVALUE, $decimalVALUE) ?>">
         <br><br>
         <label for="binary">Binary</label>
-        <input type="number" name="binary" value="<?php inputvalue("binary")?>">
+        <input type="number" name="binary" value="<?php inputvalue("binary", $binaryVALUE, $decimalVALUE) ?>">
         <br><br>
         <input type="submit">
     </form>
-
-    <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-     if (!isset($_POST["decimal"]) && !isset($_POST["binary"]) || empty($_POST["decimal"]) && empty($_POST["binary"])) {
-        echo "Empty values";
-    }elseif (!empty($_POST["decimal"]) && !empty($_POST["binary"])) {
-    echo "Least one value must be empty";
-    }
-    elseif(empty($_POST["decimal"])){
-            if(empty(str_replace(['0','1'],'', $_POST["binary"]))){
-            echo $_POST["binary"] . " equival a ". binaryTOdecimal($_POST["binary"]);
-            }
-            else
-            {
-                echo "Invalid binary number";
-            }
-        }
-        elseif(empty($_POST["binary"])){
-            echo $_POST["decimal"] . " equival a ". decimalTObinary($_POST["decimal"]);
-        }
-     }
-
-
-function decimalTObinary($value){
-    return decbin($value);
-}
-
-function binaryTodecimal($value){
-    return bindec($value);
-}
-
-function inputvalue($type){
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    echo $_POST[$type];
-    }
-}
-     ?>
 </body>
 
 </html>
